6ca210e6daf35ffd892959fbc0ae26d743d77f65
Giovanna Garcia <ggarciabas@gmail.com>
2019-11-07 17:17:29 -0300

    Atualizando cadastro com estilos

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: "https://cli-backend.herokuapp.com", 
-    baseURL: "http://localhost:3333", 
+    baseURL: "https://cli-backend.herokuapp.com", 
+    // baseURL: "http://localhost:3333", 
     timeout: 30000
 });
 
